d95e324e54337a9c1f6d26bea7733f4e7c8a8bc0
Svpernova09 <joe@joeferguson.me>
2014-12-14 13:30:45 -0600

    Add homestead

---
+++ .env.local.php
@@ -0,0 +1,8 @@
+<?php return array (
+    'DB_HOST' => 'localhost',
+    'DB_NAME' => 'homestead',
+    'DB_USERNAME' => 'homestead',
+    'DB_PASSWORD' => 'secret',
+    'DEBUG' => true,
+    'SECRET_KEY' => 'YourSecretKey!!!'
+);
