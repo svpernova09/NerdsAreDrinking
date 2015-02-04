7d638e55be0fc368513aa14363fcc2cc7e6e1f7b
Svpernova09 <joe@joeferguson.me>
2015-02-04 11:02:59 -0600

    Nuke everything

--- .env.testing.php
+++
@@ -1,8 +0,0 @@
-<?php return array (
-    'DB_HOST' => 'localhost',
-    'DB_NAME' => 'homestead',
-    'DB_USERNAME' => 'travis',
-    'DB_PASSWORD' => '',
-    'DEBUG' => true,
-    'SECRET_KEY' => 'YourSecretKey!!!'
-);
