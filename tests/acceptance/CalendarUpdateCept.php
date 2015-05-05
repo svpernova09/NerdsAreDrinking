958e8019475ecd1a3fa5e1297eefdcb589491658
Joe Ferguson <joe@joeferguson.me>
2015-05-05 08:06:29 -0500

    Disable calendar command test, need a better way to test time based things

--- tests/acceptance/CalendarUpdateCept.php
+++ tests/acceptance/CalendarUpdateCept.php
@@ -1,5 +1,7 @@
 <?php
-$I = new AcceptanceTester($scenario);
-$I->wantTo('process tweets looking for beers');
-$I->runShellCommand('php artisan nerds:cal test');
-$I->seeInShellOutput('Not time to update');
+//$I = new AcceptanceTester($scenario);
+//$I->wantTo('process tweets looking for beers');
+//$I->runShellCommand('php artisan nerds:cal test');
+//$I->seeInShellOutput('Not time to update');
+
+// Need a better way to test since this is time based
