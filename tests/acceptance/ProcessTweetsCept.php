e9e7d003103b70d76ec4684016fa2074029ceb52
Joe Ferguson <joe@joeferguson.me>
2015-05-05 22:52:44 -0500

    enable run shell command, but don't assert

--- tests/acceptance/ProcessTweetsCept.php
+++ tests/acceptance/ProcessTweetsCept.php
@@ -1,5 +1,5 @@
 <?php 
-//$I = new AcceptanceTester($scenario);
-//$I->wantTo('process tweets looking for beers');
-//$I->runShellCommand('php artisan nerds:process test');
+$I = new AcceptanceTester($scenario);
+$I->wantTo('process tweets looking for beers');
+$I->runShellCommand('php artisan nerds:process test');
 //$I->seeInShellOutput('We should have tweeted:');
