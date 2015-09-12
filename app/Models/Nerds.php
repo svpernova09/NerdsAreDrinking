8c247df94665842e6cd696007addeeb43e79c3b2
Joe Ferguson <joe@joeferguson.me>
2015-09-12 17:08:35 -0500

    rebuild app on laravel 5.1

---
+++ app/Models/Nerds.php
@@ -0,0 +1,8 @@
+<?php namespace App\Models;
+
+class Nerds extends \Eloquent {
+	protected $fillable = [
+		'name',
+		'twitter'
+	];
+}
---
+++ database/migrations/2014_12_14_203341_create_lasttweet_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateLasttweetTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('last_tweets', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->text('since_id');
+			$table->timestamps();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('last_tweets');
+	}
+
+}
