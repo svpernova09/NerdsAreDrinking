8734b52a4e5ddd2b9db718be5b4d6dc7d4f7e1dc
Joe Ferguson <joe@joeferguson.me>
2015-09-12 15:24:42 -0500

    Remove calendar stuff, simplify travis config

--- tests/acceptance/CalendarUpdateCept.php
+++
@@ -1,7 +0,0 @@
-<?php
-//$I = new AcceptanceTester($scenario);
-//$I->wantTo('process tweets looking for beers');
-//$I->runShellCommand('php artisan nerds:cal test');
-//$I->seeInShellOutput('Not time to update');
-
-// Need a better way to test since this is time based
