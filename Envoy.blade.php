2574ac76ebd5c6cf90b4df18fd149e61e109c9d5
Joe Ferguson <joe@joeferguson.me>
2015-09-19 21:42:34 -0500

    update composer and add envoy for deployment

---
+++ Envoy.blade.php
@@ -0,0 +1,8 @@
+@servers(['prod' => 'drunk@drunk.nerdsdrinking.com'])
+
+@task('deploy:prod', ['on' => 'prod'])
+cd /home/drunk/NerdsAreDrinking
+git pull origin master
+composer install
+php artisan migrate --force
+@endtask
