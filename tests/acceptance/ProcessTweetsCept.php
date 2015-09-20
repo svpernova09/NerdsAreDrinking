6b28d033925e26c804c30e49de604059bc79b78d
Joe Ferguson <joe@joeferguson.me>
2015-09-19 21:45:19 -0500

    update tests based on new debug output

--- tests/acceptance/ProcessTweetsCept.php
+++ tests/acceptance/ProcessTweetsCept.php
@@ -2,4 +2,10 @@
 $I = new AcceptanceTester($scenario);
 $I->wantTo('process tweets looking for beers');
 $I->runShellCommand('php artisan nerds:process test');
-$I->seeInShellOutput('We should have tweeted:');
+$I->seeInShellOutput('Processing thirstyrunner');
+$I->seeInShellOutput('Processing svpernova09');
+$I->seeInShellOutput('Processing markonthebluffs');
+$I->seeInShellOutput('Processing Syliddar');
+$I->seeInShellOutput('Processing sanseref');
+$I->seeInShellOutput('Processing dan9186');
+$I->seeInShellOutput('Processing vongrippen');
