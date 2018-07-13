8aea0e04be34290a116bd7063da4af205fb5c444
Joe Ferguson <joe@joeferguson.me>
2018-07-13 14:29:33 -0400

    🚧 Adding missing changes

--- after.sh
+++ after.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-mysql -u homestead -psecret -e "GRANT ALL PRIVILEGES ON homestead.* TO 'travis'@'%' IDENTIFIED BY ''"
+#mysql -u homestead -psecret -e "GRANT ALL PRIVILEGES ON homestead.* TO 'travis'@'%' IDENTIFIED BY ''"
 /usr/local/bin/composer self-update
 echo "" >> /home/vagrant/.bashrc
 echo "PATH=$PATH:/home/vagrant/nerdsaredrinking/vendor/bin" >> /home/vagrant/.bashrc
